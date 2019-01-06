a3ac70fac78d02ab6f35ad29ff87fa575bacd88a
Maartje Eyskens <meyskens@me.com>
2019-01-06 18:35:09 +0100

    Make port configurable

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/labstack/echo"
 )
 
+type config struct {
+	Port string `default:"80"`
+}
+
+var conf config
+
 func main() {
+	err := envconfig.Process("ypserver", &conf)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Any("/icecast", icecastHandle)
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":" + conf.Port))
 }
